db4aebc209ef3be06d33f1db1da539fba75b008a
ZSFan <123727427+Zsfan1224@users.noreply.github.com>
2024-07-18 18:14:25 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ let mytoken = 'Zsf13184151020'; //可以随便取，或者uuid生成，https://1
 let BotToken =''; //可以为空，或者@BotFather中输入/start，/newbot，并关注机器人
 let ChatID =''; //可以为空，或者@userinfobot中获取，/start
 let TG = 0; //小白勿动， 开发者专用，1 为推送所有的访问信息，0 为不推送订阅转换后端的访问信息与异常访问
-let FileName = 'CF-Workers-SUB';
+let FileName = 'Cloudflare-SUB-ZiJian';
 let SUBUpdateTime = 10000; //自定义订阅更新时间，单位小时
 let total = 352;//GB
 let timestamp = 1724648076000;//2024-8-26
@@ -14,7 +14,6 @@ let timestamp = 1724648076000;//2024-8-26
 let MainData = `
 vless://d342d11e-d424-4583-b36e-524ab1f0afa4@1224.us.kg:443?path=%2F%3Fed%3D2048&security=tls&encryption=none&host=vpn.zsfan.us.kg&fp=randomized&type=ws&sni=vpn.zsfan.us.kg#1224,
 vless://d342d11e-d424-4583-b36e-524ab1f0afa4@fbi.gov:443?path=%2F%3Fed%3D2048&security=tls&encryption=none&host=vpn.zsfan.us.kg&fp=randomized&type=ws&sni=vpn.zsfan.us.kg#FBI
-https://url.2332.us.kg/sub/89b3cbba-e6ac-485a-9481-976a0415eab9#BPB-Normal
 `
 
 let urls = [];
